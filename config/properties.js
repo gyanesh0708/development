c4e30eb7f58475a2f4ad0d41b87925761edc68b4
gyanesh-amm <gyanesh.saikhedkar@amm-group.com>
2019-12-30 09:18:00 +0530

    Property update

--- config/properties.js
+++ config/properties.js
@@ -1,6 +1,5 @@
 module.exports = {
     port: 3000,
-    //dbUrl: 'mongodb+srv://gyan-mongo:gyan-mongo@cluster0-ypwxx.gcp.mongodb.net/test?retryWrites=true&w=majority'
     dbUrl: 'mongodb://localhost:27017/hotel'
 
 }
